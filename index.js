90a4eb1ee317d452c99a70e2b6db0513e4e717d1
Arpit078 <verma.arpit078@.com>
2022-06-12 16:07:17 +0530

    some changes so that news does not repeat

--- index.js
+++ index.js
@@ -42,63 +42,62 @@ app.get('/Headlines',(req,res)=>{
     }).catch((err)=> console.log(err))
 })
 
-app.get('/Business-News',(req,res)=>{
-    axios.get(`https://www.businessinsider.in/business`)
+app.get('/Business',(req,res)=>{
+    axios.get(`https://economictimes.indiatimes.com/news/international/business`)
     .then((response)=>{
         const html = response.data
         const $ = cheerio.load(html)//$(".itemListElement")
         
-        $("section.list-bottom-story").each((index,element)=>{
-            const title = $(element).find('h2').text()
-            const description = $(element).find('.list-bottom-text').text()
-            const url_body = $(element).find('a').attr('href')
-            const newsUrl = "https://www.businessinsider.in/business"+url_body
+        $(".eachStory").each((index,element)=>{
+            const title = $(element).children('h3').first().text()
+            const description = $(element).children('p').first().text()
+            const url_body = $(element).children('span').first().attr('href')
+            const newsUrl = "https://economictimes.indiatimes.com/"+url_body
             const imageUrl = $(element).find('img.lazy').attr('data-original')
             let count = numbers[index]
             article[index] = {title,description,newsUrl,imageUrl,count}
         })
-        res.json({articles :article.slice(0,7)})
+        res.json({articles : article.slice(0,7)})
 
 
     }).catch((err)=> console.log(err))
 })
-app.get('/Startup-News',(req,res)=>{
-    axios.get(`https://www.businessinsider.in/business/startups`)
+app.get('/Startup',(req,res)=>{
+    axios.get(`https://economictimes.indiatimes.com/small-biz/entrepreneurship`)
     .then((response)=>{
         const html = response.data
         const $ = cheerio.load(html)//$(".itemListElement")
         
-        $("section.list-bottom-story").each((index,element)=>{
-            const title = $(element).find('h2').text()
-            const   description = $(element).find('.list-bottom-text').text()
-            const url_body = $(element).find('a').attr('href')
-            const newsUrl = "https://www.businessinsider.in/business"+url_body
+        $(".eachStory").each((index,element)=>{
+            const title = $(element).children('h3').first().text()
+            const description = $(element).children('p').first().text()
+            const url_body = $(element).children('span').first().attr('href')
+            const newsUrl = "https://economictimes.indiatimes.com/"+url_body
             const imageUrl = $(element).find('img.lazy').attr('data-original')
             let count = numbers[index]
             article[index] = {title,description,newsUrl,imageUrl,count}
         })
-        res.json({articles :article.slice(0,7)})
+        res.json({articles : article.slice(0,7)})
 
 
     }).catch((err)=> console.log(err))
 })
-app.get('/Book-Suggestions-Daily',(req,res)=>{
-    axios.get(`https://bookfortoday.com/`)
+app.get('/Finance',(req,res)=>{
+    axios.get(`https://economictimes.indiatimes.com/industry/banking-/-finance/banking`)
     .then((response)=>{
         const html = response.data
         const $ = cheerio.load(html)//$(".itemListElement")
         
-        $("article.blog-entry").each((index,element)=>{
-            const title = $(element).find('img').attr('alt')
-            // const description = null
-            const description = null
-            const url_body = $(element).find('a').attr('href')
-            const newsUrl = url_body
-            const imageUrl = $(element).find('img').attr('src')
-            let count = numbers[index]+"book"
+        $(".eachStory").each((index,element)=>{
+            const title = $(element).children('h3').first().text()
+            const description = $(element).children('p').first().text()
+            const url_body = $(element).children('span').first().attr('href')
+            const newsUrl = "https://economictimes.indiatimes.com/"+url_body
+            const imageUrl = $(element).find('img.lazy').attr('data-original')
+            let count = numbers[index]
             article[index] = {title,description,newsUrl,imageUrl,count}
         })
-        res.json({articles :article.slice(0,8)})
+        res.json({articles : article.slice(0,7)})
 
 
     }).catch((err)=> console.log(err))
